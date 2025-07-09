7e67c123029fcec48c49a1f986260eb2622f5d68
mohammed-fedaily <mohammed.fedaily@le-campus-numerique.fr>
2025-07-09 16:45:58 +0200

    add: Get PostgreSQL running with Docker

---
+++ backend/src/main/java/com/opossum/OpossumApplication.java
@@ -0,0 +1,12 @@
+package com.opossum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpossumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpossumApplication.class, args);
+    }
+}
